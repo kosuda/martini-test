b7b381d5ec5e75629361ab2b8eb2663a4e0f5dfd
kosuda_tatsuya <tatsuya.kosuda@gmail.com>
2014-08-19 19:47:46 +0900

    test

--- db/db.go
+++ db/db.go
@@ -2,7 +2,6 @@ package db
 
 import (
 	"github.com/garyburd/redigo/redis"
-	"net/url"
 	"os"
 )
 
@@ -14,13 +13,7 @@ const (
 
 func init() {
 
-	if s := os.Getenv("REDISTOGO_URL"); s != "" {
-		u, _ := url.Parse(s)
-		conn, _ = redis.Dial("tcp", u.Host)
-		p, _ := u.User.Password()
-		conn.Do("AUTH", p)
-
-	} else if s := os.Getenv("WERCKER_REDIS_HOST"); s != "" {
+	if s := os.Getenv("WERCKER_REDIS_HOST"); s != "" {
 		conn, _ = redis.Dial("tcp", s+":6379")
 
 	} else {
